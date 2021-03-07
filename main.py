e1570a384e08f40c3b571a971839c057f40944d5
liptakpanna <liptakpanna@gmail.com>
2021-03-07 10:45:13 +0100

    Import correction

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import runDiscountTest
+import runTrain
 import runAccTest
 
 discounts = [0.05,0.5,0.75,0.95]
 sizes = [100,200,300,400,500]
 for s in sizes:
     for d in discounts:
-        result = runDiscountTest.run(d, s, 0.25, 0.25)
+        result = runTrain.run(d, s, 0.25, 0.25)
 
 '''
-policy = runDiscountTest.runForAcc(0.95,100, 0.25, 0.25)
+policy = runTrain.runForAcc(0.95,100, 0.25, 0.25)
 runAccTest.acc("pectinate", policy)
 runAccTest.acc("balanced", policy)
 runAccTest.acc("random", policy)
