ebcb5cbc1ec9c3731772de9eee0b1b24664fb3b0
liptakpanna <liptakpanna@gmail.com>
2021-03-12 16:46:17 +0100

    Main updated

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import runAccTest
 
 discounts = [0.05,0.5,0.75,0.95]
 sizes = [100,200,300,400,500]
+
+result = runTrain.run(400, 0.75, 0.25, 0.25)
+result = runTrain.run(400, 0.95, 0.25, 0.25)
+for s in sizes:
+    result = runTrain.run(500, s, 0.25, 0.25)
+
+'''
+discounts = [0.05,0.5,0.75,0.95]
+sizes = [100,200,300,400,500]
 for s in sizes:
     for d in discounts:
         result = runTrain.run(d, s, 0.25, 0.25)
 
-'''
 policy = runTrain.runForAcc(0.95,100, 0.25, 0.25)
 runAccTest.acc("pectinate", policy)
 runAccTest.acc("balanced", policy)
