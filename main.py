e5336667a1b0c3a6d5142454637d927d9f42340c
Panna Lipták <liptakpanna@gmail.com>
2021-05-03 16:49:27 +0200

    LSTM layers

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import runTrain
 import runAccTest
 import time
 import dendropy
-
+'''
 tns = dendropy.TaxonNamespace()
 start_time = time.time()
 for i in range(1,100):
@@ -14,9 +14,8 @@ for i in range(1,100):
 print("--- %s seconds ---" % (time.time() - start_time))
 
 
-'''
 discount = 0.95
-sizes = [100,200,300]
+sizes = [100,300,500]
 balP = [0.1,0.2]
 pecP = [0.4,0.3]
 
@@ -28,14 +27,16 @@ for s in sizes:
             runAccTest.acc("balanced", policy)
             runAccTest.acc("random", policy)
 
+'''
 
-discounts = [0.05,0.5,0.75,0.95]
-sizes = [100,200,300,400,500]
+#discounts = [0.05,0.5,0.75,0.95]
+discounts = [0.5,0.95]
+sizes = [100,300,500]
 for s in sizes:
     for d in discounts:
         result = runTrain.run(d, s, 0.25, 0.25)
 
-
+'''
 policy = runTrain.runForAcc(0.95,100, 0.25, 0.25)
 runAccTest.acc("pectinate", policy)
 runAccTest.acc("balanced", policy)
--- runTrain.py
+++ runTrain.py
@@ -11,6 +11,7 @@ from tf_agents.policies import random_tf_policy
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.trajectories import trajectory
 from tf_agents.trajectories import policy_step
+from tf_agents.specs import tensor_spec
 from tf_agents.utils import common
 import datetime
 
@@ -81,12 +82,26 @@ def run(discount = 0.95, size=100, balP = 0.25, pecP = 0.25):
     eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
 
 
-    fc_layer_params = (100,)
+    fc_layer_params = (100,50)
 
-    q_net = q_network.QNetwork(
-      train_env.observation_spec(),
-      train_env.action_spec(),
-      fc_layer_params=fc_layer_params)
+    action_tensor_spec = tensor_spec.from_spec(train_env.action_spec())
+    num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
+
+    # Define a helper function to create Dense layers configured with the right
+    # activation and kernel initializer.
+    def dense_layer(num_units):
+      return tf.keras.layers.Dense(
+          num_units,
+          activation=tf.keras.activations.relu,
+          kernel_initializer=tf.keras.initializers.VarianceScaling(
+              scale=2.0, mode='fan_in', distribution='truncated_normal'))
+
+    # QNetwork consists of a sequence of Dense layers followed by a dense layer
+    # with `num_actions` units to generate one q_value per available action as
+    # it's output.
+    dense_layers = [dense_layer(num_units) for num_units in fc_layer_params]
+    q_values_layer = tf.keras.layers.LSTM(num_actions)
+    q_net = sequential.Sequential(dense_layers + [q_values_layer])
 
     optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
 
